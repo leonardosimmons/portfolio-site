e2747d59694a9a67af8956d5871040b97dbd5ca4
Leonardo Simmons <leonardo.simmons07@gmail.com>
2021-05-12 15:03:16 -0700

    added

---
+++ src/components/link/NamedLink.tsx
@@ -0,0 +1,20 @@
+
+import Link from 'next/link';
+import React from 'react';
+import { NamedLink, Styles } from '../../utils/types';
+
+
+type Props = NamedLink & Styles;
+
+
+const namedLink: React.FunctionComponent<Props> = ({ name, link, styles, classes }): JSX.Element => {
+  return (
+    <Link href={link}>
+      <p className={`${ styles.namedLink } ${ classes ? classes : '' }`}>
+        {name}
+      </p> 
+    </Link>
+  )
+};
+
+export default namedLink;
