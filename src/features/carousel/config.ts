2c76f04aa0028d3f448242f744a99c598614d997
Leonardo Simmons <leonardo.simmons07@gmail.com>
2021-05-14 13:40:19 -0700

    added

---
+++ src/features/carousel/config.ts
@@ -0,0 +1,9 @@
+
+export function base(active: boolean, index: number) {
+  return `${ active ? 'var(--grey-900)' : 'var(--black)' }`;
+};
+
+
+export function index(active: boolean, index: number) {
+  return `${active ? index === 0 ? 'var(--secondary-dark)' : index === 1 ? 'var(--primary-base)' : index === 2 ? 'var(--tertiary-light)' : 'var(--black)' : 'var(--black)' }`;
+};
