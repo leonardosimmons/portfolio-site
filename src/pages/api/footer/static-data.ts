8736d950d7a80e20a9ae138757ad3b16df2dc75e
Leonardo Simmons <leonardo.simmons07@gmail.com>
2021-05-21 11:33:30 -0700

    added

---
+++ src/pages/api/footer/static-data.ts
@@ -0,0 +1,21 @@
+
+import { NextApiRequest, NextApiResponse } from 'next';
+import { Footer } from '../../../utils/types';
+
+
+export default function footerData(_: NextApiRequest, res: NextApiResponse<Footer>): void
+{
+  res.status(200).json
+  (
+    {
+      links:
+      [
+        { name: 'About', link: '/about'},
+        { name: 'Portfolio', link: '/portfolio'},
+        { name: 'Contact', link: '/contact'},
+      ],
+      logo: { src: '/images/png/logo.png', alt: 'logo', width: 100, height: 100},
+      copyright: 'Built by Leonardo Simmons.'
+    }
+  )
+};
