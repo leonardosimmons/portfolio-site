f654c41bb8d539f595d63e377081af1370d5d7fe
Leonardo Simmons <leonardo.simmons07@gmail.com>
2022-10-01 13:24:05 -0700

    removed ALL original files

--- src/store/action-types.ts
+++
@@ -1,9 +0,0 @@
-
-import { ThunkAction } from 'redux-thunk';
-import { ContactMeFormActions } from '../containers/pages/contact/action-types';
-import { AppState } from './reducers';
-
-
-export type AppActions = ContactMeFormActions;
-
-export type AppThunk = ThunkAction<void, AppState, unknown, AppActions>;
