0543639173f4b5d24350d9ec73fe7a4ed7c24c15
Leonardo Simmons <leonardo.simmons07@gmail.com>
2022-10-08 00:51:43 -0700

    switched core Http methods to static

--- src/app/hooks.ts
+++ src/app/hooks.ts
@@ -1,7 +1,7 @@
-import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
-import { AppDispatch, RootState } from "./store";
-import { AxiosRequestConfig } from "axios";
-import { Http } from "../../lib/http";
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
+import { AppDispatch, RootState } from './store';
+import { AxiosRequestConfig } from 'axios';
+import { Http } from '../../lib/http';
 
 /** Dispatches predefined actions to alter application state */
 export const useAppDispatch = () => useDispatch<AppDispatch>();
@@ -13,55 +13,34 @@ export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
  * Hook for making http requests
  */
 export function useHttp() {
-    const http = new Http();
+  /** Performs a http 'GET' request */
+  async function get<D>(api: string, config?: AxiosRequestConfig): Promise<D> {
+    return await Http.get(api, config);
+  }
 
-    /** Performs a http 'GET' request */
-    async function get<D>(
-        api: string,
-        config?: AxiosRequestConfig
-    ): Promise<D> {
-        try {
-            return await http.get(api, config);
-        } catch (err) {
-            throw err;
-        }
-    }
+  /** Performs a http 'POST' request */
+  async function post<D>(
+    api: string,
+    data: D,
+    config?: AxiosRequestConfig,
+  ): Promise<D> {
+    return await Http.post(api, data, config);
+  }
 
-    /** Performs a http 'POST' request */
-    async function post<D>(
-        api: string,
-        data: D,
-        config?: AxiosRequestConfig
-    ): Promise<D> {
-        try {
-            return await http.post(api, data, config);
-        } catch (err) {
-            throw err;
-        }
-    }
+  /** Performs a http 'PUT' request */
+  async function put<D>(
+    api: string,
+    data: D,
+    config?: AxiosRequestConfig,
+  ): Promise<D> {
+    return await Http.put(api, data, config);
+  }
 
-    /** Performs a http 'PUT' request */
-    async function put<D>(
-        api: string,
-        data: D,
-        config?: AxiosRequestConfig
-    ): Promise<D> {
-        try {
-            return await http.put(api, data, config);
-        } catch (err) {
-            throw err;
-        }
-    }
-
-    /** Performs a http 'DELETE' request */
-    async function remove<D>(
-        api: string,
-        config?: AxiosRequestConfig
-    ): Promise<D> {
-        try {
-            return await http.remove(api, config);
-        } catch (err) {
-            throw err;
-        }
-    }
+  /** Performs a http 'DELETE' request */
+  async function remove<D>(
+    api: string,
+    config?: AxiosRequestConfig,
+  ): Promise<D> {
+    return await Http.remove(api, config);
+  }
 }
