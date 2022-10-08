13a00feee83fe92024435bd5ed734640c862099a
Leonardo Simmons <leonardo.simmons07@gmail.com>
2022-10-08 11:41:33 -0700

    base setup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,31 @@
 import React from 'react';
 import { GetStaticProps, InferGetStaticPropsType } from 'next';
+import { HomePageData } from '../../lib/types';
+import { Http } from '../../lib/http';
 
 import Layout from '../../lib/components/layout/Layout';
+import Heading from '../../lib/components/heading/Heading';
 
 export default function Index({
-  pageTitle,
-  title,
+  data,
 }: InferGetStaticPropsType<typeof getStaticProps>): JSX.Element {
+  const [page] = React.useState<HomePageData>(data);
+
   return (
-    <Layout title={pageTitle}>
-      <div>{title}</div>
+    <Layout title={data.metaTitle}>
+      <Heading {...page.heading.main} />
     </Layout>
   );
 }
 
-export const getStaticProps: GetStaticProps = async () => {
+const { HOME_PAGE_DATA } = process.env;
+
+export const getStaticProps: GetStaticProps = async (_) => {
+  const data = await Http.get<HomePageData>(HOME_PAGE_DATA as string);
+
   return {
     props: {
-      pageTitle:
-        'Full Stack Web Developer & Software Engineer Portland, Oregon',
-      title: 'Welcome to Leonardo Simmons portfolio site!',
+      data,
     },
   };
 };
