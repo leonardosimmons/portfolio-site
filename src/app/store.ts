06e895332a48f07cda4b369ce32aa69af1bac72e
Leonardo Simmons <leonardo.simmons07@gmail.com>
2022-10-09 01:56:22 -0700

    added Ui & Api state

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,24 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore } from '@reduxjs/toolkit';
+import { createWrapper } from 'next-redux-wrapper';
 
-export const store = configureStore({
-    reducer: {},
-    devTools: process && process.env.NODE_ENV === "development",
+import { dataApi } from './data';
+import uiReducer from '../features/ui/ui-slice';
+
+export const store = () =>
+  configureStore({
+    reducer: {
+      ui: uiReducer,
+      [dataApi.reducerPath]: dataApi.reducer,
+    },
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware().concat(dataApi.middleware),
+    devTools: process && process.env.NODE_ENV === 'development',
+  });
+
+export type AppStore = ReturnType<typeof store>;
+export type RootState = ReturnType<AppStore['getState']>;
+export type AppDispatch = AppStore['dispatch'];
+
+export const AppState = createWrapper<AppStore>(store, {
+  debug: true,
 });
-
-export type AppDispatch = typeof store.dispatch;
-export type RootState = ReturnType<typeof store.getState>;
