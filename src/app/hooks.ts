73e1f66a2205402e5e5d0414cd65a991294c2493
Leonardo Simmons <leonardo.simmons07@gmail.com>
2022-10-09 01:56:01 -0700

    removed useHttp & added useViewport

--- src/app/hooks.ts
+++ src/app/hooks.ts
@@ -1,7 +1,7 @@
+import React from 'react';
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 import { AppDispatch, RootState } from './store';
-import { AxiosRequestConfig } from 'axios';
-import { Http } from '../../lib/http';
+import { updateCurrentWindowSize } from '../features/ui/ui-slice';
 
 /** Dispatches predefined actions to alter application state */
 export const useAppDispatch = () => useDispatch<AppDispatch>();
@@ -9,38 +9,22 @@ export const useAppDispatch = () => useDispatch<AppDispatch>();
 /** Application state selector */
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 
-/**
- * Hook for making http requests
- */
-export function useHttp() {
-  /** Performs a http 'GET' request */
-  async function get<D>(api: string, config?: AxiosRequestConfig): Promise<D> {
-    return await Http.get(api, config);
+/** Sets current viewport size */
+export function useViewport() {
+  const dispatch = useAppDispatch();
+
+  function handleWindowResize() {
+    dispatch(
+      updateCurrentWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      }),
+    );
   }
 
-  /** Performs a http 'POST' request */
-  async function post<D>(
-    api: string,
-    data: D,
-    config?: AxiosRequestConfig,
-  ): Promise<D> {
-    return await Http.post(api, data, config);
-  }
-
-  /** Performs a http 'PUT' request */
-  async function put<D>(
-    api: string,
-    data: D,
-    config?: AxiosRequestConfig,
-  ): Promise<D> {
-    return await Http.put(api, data, config);
-  }
-
-  /** Performs a http 'DELETE' request */
-  async function remove<D>(
-    api: string,
-    config?: AxiosRequestConfig,
-  ): Promise<D> {
-    return await Http.remove(api, config);
-  }
+  React.useEffect(() => {
+    window.addEventListener('resize', handleWindowResize);
+    handleWindowResize();
+    return () => window.removeEventListener('resize', handleWindowResize);
+  }, []);
 }
