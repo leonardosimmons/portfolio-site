57ffb377dfdfb3be1c2355c0de62b6e5d2b6c56a
Leonardo Simmons <leonardo.simmons07@gmail.com>
2022-10-10 02:02:10 -0700

    server 500 error fix | populated .env on client instead of server

--- src/app/data.ts
+++ src/app/data.ts
@@ -2,11 +2,11 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { HomePageData } from './types';
 import { HYDRATE } from 'next-redux-wrapper';
 
-const { NEXT_PUBLIC_BASE_API } = process.env;
-
 export const dataApi = createApi({
   reducerPath: 'api',
-  baseQuery: fetchBaseQuery({ baseUrl: NEXT_PUBLIC_BASE_API as string }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: process.env.NEXT_PUBLIC_BASE_API as string,
+  }),
   extractRehydrationInfo: (action, { reducerPath }) => {
     if (action.type === HYDRATE) {
       return action.payload[reducerPath];
